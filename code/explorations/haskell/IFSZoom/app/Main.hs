f07d708bddc5bc1c9ae7595ec724075b366ceb84
Marten/Qqwy <w-m@wmcode.nl>
2020-05-29 17:12:37 +0200

    Create a parallel binary search implementation
    to learn how Accelerate works
    (and as potential building block for a bounded hierarcical tree
    structure)

--- code/explorations/haskell/IFSZoom/app/Main.hs
+++ code/explorations/haskell/IFSZoom/app/Main.hs
@@ -25,13 +25,20 @@ import Data.Array.Accelerate.LLVM.Native                            as CPU
 import Data.Array.Accelerate.LLVM.PTX                               as PTX
 #endif
 
+import Data.Array.Accelerate.Array.Sugar                        as S
+
 main :: IO ()
 main = do
+
+  runExample
+  runBinarySearch
+
+runExample = do
   let
-      xs :: Vector Double
+      xs :: Vector Int
       xs = fromList (Z:.10) [0..]
 
-      ys :: Vector Double
+      ys :: Vector Int
       ys = fromList (Z:.10) [1,3..]
 
   printf "input data:\n"
@@ -47,4 +54,29 @@ main = do
 #endif
 #ifdef ACCELERATE_LLVM_PTX_BACKEND
   printf "result with PTX backend: dotp xs ys = %s\n" (show (PTX.runN dotp xs ys))
+#endif
+
+-- runBinarySearch :: (Acc (Vector Int) -> Exp Int -> Acc (Scalar Int)) -> Acc (Vector Int) -> Exp Int -> IO ()
+runBinarySearch = do
+  let
+    arr :: Acc (Vector Int)
+    arr = use $ fromList (Z:.10000) [1,3..]
+    elem = unit 122
+    acc_ast = binarySearch
+
+  printf "the function to execute:\n"
+  printf "%s\n\n" (show acc_ast)
+
+  -- printf "the input to execute:\n"
+  -- printf "%s\n\n" (show input)
+
+  printf "result with interpreter backend: %s\n" (show (Interpreter.runN $ acc_ast arr elem))
+  let
+    res = (Interpreter.runN $ acc_ast arr elem)
+  printf "result: %s\n" (show (res S.! Z))
+#ifdef ACCELERATE_LLVM_NATIVE_BACKEND
+  printf "result with CPU backend: %s\n" (show (CPU.runN $ acc_ast arr elem))
+#endif
+#ifdef ACCELERATE_LLVM_PTX_BACKEND
+  printf "result with PTX backend: %s\n" (show (PTX.runN $ acc_ast arr elem))
 #endif
--- code/explorations/haskell/IFSZoom/src/Lib.hs
+++ code/explorations/haskell/IFSZoom/src/Lib.hs
@@ -11,7 +11,8 @@
 
 module Lib (
 
-  dotp
+  dotp,
+  binarySearch
 
 ) where
 
@@ -19,5 +20,81 @@ import Data.Array.Accelerate
 
 -- | A simple vector inner product
 --
-dotp :: Acc (Vector Double) -> Acc (Vector Double) -> Acc (Scalar Double)
+dotp :: Acc (Vector Int) -> Acc (Vector Int) -> Acc (Scalar Int)
 dotp xs ys = fold (*) 1 ( zipWith (+) xs ys)
+
+
+binarySearch :: Acc (Vector Int) -> Acc (Scalar Int) -> Acc (Scalar Int)
+binarySearch arr wrapped_target =
+  let
+    target = the wrapped_target
+    index = fst $ binarySearch' arr target 0 (length arr - 1)
+  in
+    unit index
+
+binarySearch' :: Acc (Vector Int) -> Exp Int -> Exp Int -> Exp Int -> Exp (Int, Int)
+binarySearch' arr target left right =
+  let
+    initial :: Exp (Int, Int)
+    initial = liftedPair left right
+
+    check :: Exp (Int, Int) -> Exp Bool
+    check pair =
+      let
+        (left, right) = unlift pair
+      in
+        (arr !! left) < (arr !! right)
+
+    body :: Exp (Int, Int) -> Exp (Int, Int)
+    body pair =
+      let
+        (left, right) = unlift pair
+        middle = (left + right) `quot` 2
+      in
+        -- case (compare (arr !! middle) target :: Ordering) of
+        --   LT ->
+        --     liftedPair (middle + 1) right
+        --   GT ->
+        --     liftedPair left (middle - 1)
+        --   EQ ->
+        --     liftedPair middle middle
+        caseof (compare (arr !! middle) target)
+        [
+          ((== constant LT), liftedPair (middle + 1) right),
+          ((== constant GT), liftedPair left (middle - 1))
+        ]
+        (liftedPair middle middle)
+
+  in
+    while check body initial
+  -- let
+  --   check :: Exp (DIM1, DIM1) -> Exp Bool
+  --   check (left, right) = (arr ! left) < (arr ! right)
+  -- in
+  --   while _
+  --   (\tuple ->
+  --     let
+  --       (left, right) = unlift tuple :: (Exp DIM1, Exp DIM1)
+  --       middle :: Exp DIM1
+  --       middle = (left + right) `div` 2
+  --   in
+  --       _
+  --     -- lift $ case compare (arr ! middle) target of
+  --     --   LT ->
+  --     --     (middle + 1, right)
+  --     --   GT ->
+  --     --     (left, middle - 1)
+  --     --   EQ ->
+  --     --     (middle, middle)
+  --     )
+  --   (unlift (left, right))
+
+-- arrsize :: Acc (Vector Int) -> Exp DIM1
+-- arrsize arr = lift $ (\i -> Z :. i) $ length $ arr
+
+liftedPair :: Exp Int -> Exp Int -> Exp (Int, Int)
+liftedPair a b =
+  let
+    pair = (a, b) :: (Exp Int, Exp Int)
+  in
+    lift pair
