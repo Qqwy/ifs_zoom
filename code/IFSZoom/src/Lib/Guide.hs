22568323c834e818d79a78141333b4cd3bbf7501
Marten/Qqwy <w-m@wmcode.nl>
2020-07-25 23:27:50 +0200

    Refactoring; getting ready to draw guides for multiple layers of
    transformations

--- code/IFSZoom/src/Lib/Guide.hs
+++ code/IFSZoom/src/Lib/Guide.hs
@@ -5,23 +5,34 @@ module Lib.Guide
 
 import Pipe
 import qualified Lib
+import qualified Lib.Common
+import Lib.Common (Transformation)
 import qualified Lib.Camera
 import qualified Graphics.Gloss as Gloss
 import qualified Graphics.Gloss.Data.Picture
 
-drawGuides :: Lib.Camera -> Lib.Camera -> (Word, Word) -> Gloss.Picture
+drawGuides :: Lib.Camera -> Lib.Camera -> (Word, Word) ->  Gloss.Picture
 drawGuides camera initial_camera dimensions =
-  drawGuide (0, 0, 1, 1) camera initial_camera dims
+  drawGuide camera initial_camera dims []
   |> Graphics.Gloss.Data.Picture.color Gloss.red
   where
     dims = dimensions |> (\(x, y) -> (fromIntegral x, fromIntegral y))
 
-drawGuide :: (Float, Float, Float, Float) -> Lib.Camera -> Lib.Camera -> (Float, Float) -> Gloss.Picture
-drawGuide (x, y, w, h) camera initial_camera (pw, ph) =
-  -- Graphics.Gloss.Data.Picture.blank
+drawGuide :: Lib.Camera -> Lib.Camera -> (Float, Float) -> [Transformation] -> Gloss.Picture
+drawGuide camera initial_camera dimensions transformations =
+  (0, 0, 1, 1)
+  |> guideFromCoords initial_camera
+  |> guideToPicture camera dimensions
+
+guideFromCoords :: Lib.Camera ->(Float, Float, Float, Float) ->  [(Float, Float)]
+guideFromCoords initial_camera (x, y, w, h)  =
   [(x, y), (x+w, y), (x+w, y+h), (x, y+h), (x, y)]
   |> fmap (Lib.Camera.cameraTransform (Lib.Camera.inverseCamera initial_camera))
+
+guideToPicture :: Lib.Camera -> (Float, Float) -> [(Float, Float)] -> Gloss.Picture
+guideToPicture camera (screen_width, screen_height) guide =
+  guide
   |> fmap (Lib.Camera.cameraTransform camera)
-  |> fmap (\(x, y) -> (x*pw, y*(-ph)))
+  |> fmap (\(x, y) -> (x*screen_width, y*(-screen_height)))
   |> Graphics.Gloss.Data.Picture.line
-  |> Graphics.Gloss.Data.Picture.translate (-pw/2) (ph/2)
+  |> Graphics.Gloss.Data.Picture.translate (-screen_width/2) (screen_height/2)
