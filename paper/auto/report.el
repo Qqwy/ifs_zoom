258aba679ab3b2786f2e5d48bfa5059da7a6d035
Marten/Qqwy <w-m@wmcode.nl>
2020-08-11 14:01:42 +0200

    Adds 'rugscriptie' package.

--- paper/auto/report.el
+++ paper/auto/report.el
@@ -5,13 +5,13 @@
                      '(("article" "11pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem") ("geometry" "a4paper" "total={7in, 9in}") ("xcolor" "dvipsnames") ("enumitem" "shortlabels")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -37,20 +37,21 @@
     "subcaption"
     "enumitem"
     "newfloat"
-    "todonotes")
+    "todonotes"
+    "rugscriptie")
    (TeX-add-symbols
     "cmark"
     "xmark")
    (LaTeX-add-labels
-    "sec:org9a2c21b"
-    "sec:orgd6db77a"
-    "sec:org6b595ce"
-    "sec:orgd4fe10a"
-    "sec:orgb2ab338"
-    "sec:org08a0eac"
-    "sec:org914fb67"
-    "sec:org58f533e"
-    "sec:org52978d3")
+    "sec:org853776f"
+    "sec:orgb0cdd90"
+    "sec:org5c6eeb8"
+    "sec:orgc91d23c"
+    "sec:org60f7c03"
+    "sec:org767e78c"
+    "sec:orgb94cebb"
+    "sec:orgfc1a3a4"
+    "sec:org2d3b322")
    (LaTeX-add-bibliographies
     "bibliography")
    (LaTeX-add-newfloat-DeclareFloatingEnvironments
