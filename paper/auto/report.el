ab01373f57f4b5b91987f51ce64d60a2f2bc8875
Marten/Qqwy <w-m@wmcode.nl>
2020-08-11 15:42:32 +0200

    Lot more writing

--- paper/auto/report.el
+++ paper/auto/report.el
@@ -5,13 +5,13 @@
                      '(("article" "11pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem") ("geometry" "a4paper" "total={7in, 9in}") ("xcolor" "dvipsnames") ("enumitem" "shortlabels")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -43,15 +43,19 @@
     "cmark"
     "xmark")
    (LaTeX-add-labels
-    "sec:org853776f"
-    "sec:orgb0cdd90"
-    "sec:org5c6eeb8"
-    "sec:orgc91d23c"
-    "sec:org60f7c03"
-    "sec:org767e78c"
-    "sec:orgb94cebb"
-    "sec:orgfc1a3a4"
-    "sec:org2d3b322")
+    "sec:org765d11f"
+    "sec:org9d7bd27"
+    "sec:orgeff9e70"
+    "sec:org0ba37d5"
+    "sec:org552e065"
+    "sec:orge005f52"
+    "sec:org12e162b"
+    "sec:org3aac92d"
+    "sec:org7074f3f"
+    "sec:orgcc42799"
+    "sec:org9938963"
+    "sec:orgae0c01f"
+    "sec:org5698720")
    (LaTeX-add-bibliographies
     "bibliography")
    (LaTeX-add-newfloat-DeclareFloatingEnvironments
