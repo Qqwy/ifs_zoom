bbeb5ffae6f658b9c293dba7558dda74219f3655
Marten/Qqwy <w-m@wmcode.nl>
2020-08-12 17:29:41 +0200

    Fix algorithm environment, explanation of the Chaos Game

--- paper/auto/report.el
+++ paper/auto/report.el
@@ -4,14 +4,14 @@
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("article" "11pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem") ("geometry" "a4paper" "total={7in, 9in}") ("xcolor" "dvipsnames") ("enumitem" "shortlabels")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem") ("geometry" "a4paper" "total={7in, 9in}") ("algorithm2e" "ruled" "procnumbered") ("xcolor" "dvipsnames") ("enumitem" "shortlabels")))
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -32,30 +32,36 @@
     "hyperref"
     "float"
     "geometry"
+    "algorithm2e"
     "xcolor"
     "pifont"
     "subcaption"
     "enumitem"
     "newfloat"
     "todonotes"
+    "kantlipsum"
     "rugscriptie")
    (TeX-add-symbols
     "cmark"
-    "xmark")
+    "xmark"
+    "altasterism")
    (LaTeX-add-labels
-    "sec:org765d11f"
-    "sec:org9d7bd27"
-    "sec:orgeff9e70"
-    "sec:org0ba37d5"
-    "sec:org552e065"
-    "sec:orge005f52"
-    "sec:org12e162b"
-    "sec:org3aac92d"
-    "sec:org7074f3f"
-    "sec:orgcc42799"
-    "sec:org9938963"
-    "sec:orgae0c01f"
-    "sec:org5698720")
+    "sec:org8eea98e"
+    "sec:org73d7cf8"
+    "sec:org588bade"
+    "sec:orgcfb86e1"
+    "sec:org17278a9"
+    "sec:org7074d15"
+    "sec:orgc8a9298"
+    "sec:org12f5657"
+    "chaosGame"
+    "sec:org6bd243f"
+    "sec:orgbb3a13b"
+    "sec:orge67e4b2"
+    "sec:org33e5105"
+    "sec:org11568aa"
+    "sec:orgc17b009"
+    "sec:orgb952e9d")
    (LaTeX-add-bibliographies
     "bibliography")
    (LaTeX-add-newfloat-DeclareFloatingEnvironments
