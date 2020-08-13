239d1dd662c6a6ea4cad17a9838405dda32f3cbd
Marten/Qqwy <w-m@wmcode.nl>
2020-08-14 00:06:27 +0200

    More thesis writing (WIP 'Implementation' part)

--- paper/auto/report.el
+++ paper/auto/report.el
@@ -5,13 +5,13 @@
                      '(("article" "11pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem") ("geometry" "a4paper" "total={7in, 9in}") ("algorithm2e" "ruled" "procnumbered") ("xcolor" "dvipsnames") ("enumitem" "shortlabels")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -46,31 +46,43 @@
     "xmark"
     "altasterism")
    (LaTeX-add-labels
-    "sec:org62e804a"
-    "sec:org0f916c5"
-    "sec:orgbcbd99d"
-    "sec:orgca3ba16"
+    "sec:orgd4eeed8"
+    "sec:org8f60ae0"
+    "sec:org6dc882d"
+    "sec:org7f1de23"
     "section:background"
-    "sec:orgc57e3e1"
-    "sec:org42f9761"
-    "sec:org98711ed"
-    "sec:org558725b"
+    "sec:org56453c0"
+    "sec:org491f2b4"
+    "sec:org23e634a"
+    "sec:org1bee90d"
     "subsection:chaos_game"
     "chaosGame"
-    "sec:orgbdd27a1"
-    "sec:org7fd41b9"
+    "sec:orgffab345"
+    "sec:org7be3eab"
     "subsection:chaos_game_gpu"
-    "sec:org86a92c4"
-    "sec:org45f035b"
-    "sec:orgb622a54"
-    "sec:org5277319"
-    "sec:org1f1a548"
-    "sec:org3924fb2"
-    "sec:orge256af4"
-    "sec:org365db71"
-    "sec:org41d4fed"
-    "sec:orgafa8544"
-    "sec:org646e738")
+    "sec:orgbf76a80"
+    "sec:orgf5f0256"
+    "sec:orgeee00e8"
+    "sec:orgc2c5418"
+    "sec:org15a778d"
+    "sec:org0070bb1"
+    "sec:org479085d"
+    "sec:orge9609da"
+    "subsection:self_similarity"
+    "sec:orgec6ca05"
+    "sec:org177fbb3"
+    "sec:org6197cdf"
+    "sec:orga0cdbc8"
+    "sec:org834aaaa"
+    "sec:orgcf7a295"
+    "sec:org4ae5fb8"
+    "sec:org1d3588c"
+    "sec:org05f1ce5"
+    "sec:org2e7bb93"
+    "sec:org4c00ae2"
+    "sec:orged143d7"
+    "sec:org73c54ea"
+    "sec:org3899552")
    (LaTeX-add-bibliographies
     "bibliography")
    (LaTeX-add-newfloat-DeclareFloatingEnvironments
