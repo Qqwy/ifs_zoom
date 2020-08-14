3c6b83ceb0cbeefae39190b4043d59f30889fffd
Marten/Qqwy <w-m@wmcode.nl>
2020-08-14 21:31:38 +0200

    Including dragon curve submapping pictures.

--- paper/auto/report.el
+++ paper/auto/report.el
@@ -5,13 +5,13 @@
                      '(("article" "11pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
                      '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem") ("geometry" "a4paper" "total={7in, 9in}") ("algorithm2e" "ruled" "procnumbered") ("xcolor" "dvipsnames") ("enumitem" "shortlabels")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "latex2e"
@@ -46,50 +46,58 @@
     "xmark"
     "altasterism")
    (LaTeX-add-labels
-    "sec:org6c020a4"
-    "sec:orgaf9ca23"
-    "sec:org80fbb4e"
-    "sec:org3aadd34"
+    "sec:orgc5b263f"
+    "sec:orge115b1a"
+    "sec:org7147e0d"
+    "sec:org32880ce"
     "section:background"
-    "sec:orgee658f7"
-    "sec:org00b805d"
-    "sec:orgafc99c3"
-    "sec:org448db85"
+    "sec:org6a0c6d4"
+    "sec:org15d6dda"
+    "sec:org8ae51b8"
+    "sec:orgf4dbce6"
     "subsection:viewport_transformation"
-    "sec:org3e92a09"
-    "sec:org0392e27"
-    "sec:org6824b9c"
+    "sec:org5bdd311"
+    "sec:orgb8298b8"
+    "sec:org80c999f"
     "subsection:chaos_game"
     "chaosGame"
-    "sec:orge2a65d9"
-    "sec:org27fb09e"
+    "sec:org668f987"
+    "sec:org4b1ea41"
     "subsection:chaos_game_gpu"
-    "sec:orgbbb02a0"
+    "sec:org2dbddc6"
     "subsection:deterministic_gpu"
-    "sec:org3184d25"
-    "sec:org01456ad"
-    "sec:org1d634cb"
-    "sec:org85e6b69"
-    "sec:org10cd42b"
+    "sec:org59cb855"
+    "sec:org3703620"
+    "sec:orgd1553e1"
+    "sec:org14a72e9"
+    "sec:org748d9ef"
     "subsection:point_cloud_optimizations"
-    "sec:org10f68ba"
+    "sec:orgcf27e48"
     "subsection:self_similarity"
-    "sec:orgb873968"
-    "sec:org8c89bd1"
-    "sec:orgaa58ee4"
-    "sec:org59e7717"
-    "sec:orgb8ffe93"
-    "sec:org67e7edc"
-    "sec:org3252c46"
-    "sec:org59b2415"
-    "sec:org0429ae8"
-    "sec:org2c2571e"
-    "sec:org38d82a8"
-    "sec:org90ebb89"
-    "sec:orgb2b82d5"
-    "sec:org09753ed"
-    "sec:org03541c9"
-    "sec:org7e5c291")
+    "sec:org61f20fb"
+    "sec:org351a067"
+    "sec:org092a439"
+    "sec:orgd1ce23b"
+    "sec:org9636adb"
+    "sec:orgb25c7f1"
+    "sec:orgdfc92ae"
+    "sec:org827203d"
+    "sec:org67a4ce7"
+    "sec:org3017905"
+    "subsection:jumping_restrictions"
+    "sec:orgee8e6c2"
+    "sec:org7181b6e"
+    "figigure:dragon_curve_a"
+    "figigure:dragon_curve_b"
+    "fig:five over x"
+    "figigure:dragon_curve_d"
+    "figure:dragon_curve_overlaps"
+    "sec:org6314dd9"
+    "subsection:coloring"
+    "sec:orge1e3bd7"
+    "sec:org96b225b"
+    "sec:orge863faf"
+    "sec:orgbcc6e9b")
    (LaTeX-add-bibliographies
     "bibliography")
    (LaTeX-add-newfloat-DeclareFloatingEnvironments
