5d3bddef33502221a3a206dad2b82ae851632805
Marten/Qqwy <w-m@wmcode.nl>
2020-08-15 02:30:04 +0200

    Fill in bibliography

---
+++ paper/auto/bibliography.el
@@ -0,0 +1,18 @@
+(TeX-add-style-hook
+ "bibliography"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "green2005gpu"
+    "jampour2010new"
+    "barnsley2014fractals"
+    "jeffrey1990chaos"
+    "barnsley2010chaos"
+    "lauterbach2009construction"
+    "hart1996fractal"
+    "mendivil2003fractals"
+    "lawlor2012gpu"
+    "wimmer2006instant"
+    "hepting1991rendering"
+    "draves2003fractal"))
+ :bibtex)
+
