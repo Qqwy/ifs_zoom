ec653d77996c2a2fbd30add1ef913f237ba78a9e
Marten/Qqwy <w-m@wmcode.nl>
2020-08-17 12:41:57 +0200

    Proofread up to 3.2.2

--- paper/auto/report.el
+++ paper/auto/report.el
@@ -4,18 +4,18 @@
    (TeX-add-to-alist 'LaTeX-provided-class-options
                      '(("article" "11pt")))
    (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem") ("geometry" "a4paper" "total={7in, 9in}") ("algorithm2e" "ruled" "procnumbered") ("xcolor" "dvipsnames") ("enumitem" "shortlabels")))
+                     '(("inputenc" "utf8") ("fontenc" "T1") ("ulem" "normalem") ("geometry" "a4paper" "total={7in, 9in}") ("caption" "font={small, it}" "labelfont=bf") ("algorithm2e" "ruled" "procnumbered") ("xcolor" "dvipsnames") ("enumitem" "shortlabels")))
    (add-to-list 'LaTeX-verbatim-environments-local "lstlisting")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
-   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "lstinline")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "lstinline")
    (TeX-run-style-hooks
     "latex2e"
     "article"
@@ -35,6 +35,7 @@
     "hyperref"
     "float"
     "geometry"
+    "caption"
     "algorithm2e"
     "listings"
     "xcolor"
@@ -50,65 +51,67 @@
     "xmark"
     "altasterism")
    (LaTeX-add-labels
-    "sec:orgf1a4fc4"
-    "sec:org3a01a6a"
-    "sec:org7628d8a"
-    "sec:orge520937"
+    "sec:orgfee208a"
+    "sec:orgfa7e82a"
+    "sec:org6c4d6bc"
+    "sec:org3b4b690"
     "section:background"
-    "sec:org14fb05b"
+    "sec:org75f0239"
     "figure:sierpinsky_iterations"
-    "sec:org3e564f1"
-    "sec:org5c55f60"
-    "sec:org37b682c"
+    "sec:orgb0012b2"
+    "sec:org0a93b42"
+    "sec:org28a3a19"
     "subsection:viewport_transformation"
-    "sec:org9c0046c"
-    "sec:org09257de"
-    "sec:orga1103be"
+    "sec:org66d5d21"
+    "sec:orga0aa444"
+    "sec:org7ff9fbd"
+    "sec:org1202c96"
+    "sec:orgd50a544"
     "subsection:chaos_game"
     "figure:barnsley_mil"
     "figure:barnsley_ten_mil"
     "figure:barnsley_chaos_game_points"
     "chaosGame"
-    "sec:orgfd24cfe"
-    "sec:orgbfb695c"
+    "sec:orgaba79a4"
+    "sec:orgdbcdfb4"
     "subsection:chaos_game_gpu"
-    "sec:org2a9f800"
+    "sec:org8530ebf"
     "subsection:deterministic_gpu"
-    "sec:org226c34e"
+    "sec:orgcd35dcb"
     "section:research_question"
-    "sec:orged310ea"
+    "sec:org674aab3"
     "section:approach"
-    "sec:org4811b68"
-    "sec:org8d83c24"
-    "sec:org1377317"
+    "sec:org1f92692"
+    "sec:org950fda9"
+    "sec:org56fe4d5"
     "subsection:point_cloud_optimizations"
-    "sec:org697e525"
+    "sec:orgb6a65a4"
     "subsection:self_similarity"
     "figure:sierpinsky_jump"
     "algorithm:self_similarity_jump_up"
     "algorithm:self_similarity_jump_down"
-    "sec:orgae40080"
+    "sec:orgfa5676f"
     "subsection:coloring"
-    "sec:org8fb5d2f"
+    "sec:org328574a"
     "figure:program_flow"
-    "sec:org650a312"
-    "sec:org197a1c5"
-    "sec:orgb7f1b2d"
+    "sec:orgce3c643"
+    "sec:org3bf8231"
+    "sec:org06ac1df"
     "listing:barnsley_fern_ifs_file"
-    "sec:org0b8df97"
-    "sec:orge3d9b9e"
-    "sec:orgaa4ba2b"
-    "sec:org20074ea"
+    "sec:org9670a41"
+    "sec:orgacf59da"
+    "sec:orgbc25dde"
+    "sec:org2e48682"
     "figure:barnsley_guides"
     "figure:barnsley_guides_and_points"
     "figure:barnsley_guides_vs_points"
-    "sec:org0ca7f19"
+    "sec:orgc22ceff"
     "section:findings"
-    "sec:org60f2bac"
+    "sec:orge992bee"
     "subsection:jumping_restrictions"
-    "sec:orgd7482bf"
+    "sec:org49c4b88"
     "figure:sierpinsky_transformation_borders"
-    "sec:org90d10f1"
+    "sec:orgc0713be"
     "figure:barnsley_jump_a"
     "figure:barnsley_jump_b"
     "figure:barnsley_jump"
@@ -117,12 +120,12 @@
     "figure:dragon_curve_c"
     "figure:dragon_curve_d"
     "figure:dragon_curve_overlaps"
-    "sec:orge8ec75e"
-    "sec:orgde192ca"
+    "sec:org0b6489a"
+    "sec:org5942d2a"
     "section:conclusion"
-    "sec:orgb4cf905"
+    "sec:org7fbfc0f"
     "section:further_work"
-    "sec:org9b21927"
+    "sec:orge17e93c"
     "ifs:sierpinsky"
     "ifs:dragon_curve"
     "ifs:barnsley_fern")
